84d89e54f30e0d7500e98656bed3bd4be94f47c6
Bolla Ramesh Babu <110804950+brams17011983@users.noreply.github.com>
2022-10-21 14:55:06 +0530

    Added testAppMain2

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -39,6 +39,16 @@ public class AppTest
             fail("\"message\" is not \"Hello World!\"");
         }
     }
+    @Test
+    public void testAppMain2()
+    {
+        App.main(null);
+        try {
+            assertEquals("Hello World!" + System.getProperty("line.separator"), outContent.toString());
+        } catch (AssertionError e) {
+            fail("\"message\" is not \"Hello World!\"");
+        }
+    }
 
     @After
     public void cleanUpStreams() {
