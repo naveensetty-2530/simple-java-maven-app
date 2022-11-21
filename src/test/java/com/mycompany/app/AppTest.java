a4095ef0b0ba25cac4558122d198b82e4b62f985
Bolla Ramesh Babu <110804950+brams17011983@users.noreply.github.com>
2022-11-21 15:47:49 +0530

    Update AppTest.java

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -1,25 +1,20 @@
 package com.mycompany.app;
-
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.After;
 import static org.junit.Assert.*;
-
 /**
  * Unit test for simple App.
  */
 public class AppTest
 {
-
     private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-
     @Before
     public void setUpStreams() {
         System.setOut(new PrintStream(outContent));
     }
-
     @Test
     public void testAppConstructor() {
         try {
@@ -28,7 +23,6 @@ public class AppTest
             fail("Construction failed.");
         }
     }
-
     @Test
     public void testAppMain()
     {
@@ -39,10 +33,8 @@ public class AppTest
             fail("\"message\" is not \"Hello World!\"");
         }
     }
-    
     @After
     public void cleanUpStreams() {
         System.setOut(null);
     }
-
 }
