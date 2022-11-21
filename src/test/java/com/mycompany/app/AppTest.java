8cb57eddfd74920b80ddbef7e13dcc080bcc6c32
Bolla Ramesh Babu <110804950+brams17011983@users.noreply.github.com>
2022-11-21 15:41:51 +0530

    Update AppTest.java

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -39,17 +39,7 @@ public class AppTest
             fail("\"message\" is not \"Hello World!\"");
         }
     }
-    @Test
-    public void testAppMain2()
-    {
-        App.main(null);
-        try {
-            assertEquals("Hello World!" + System.getProperty("line.separator"), outContent.toString());
-        } catch (AssertionError e) {
-            fail("\"message\" is not \"Hello World!\"");
-        }
-    }
-
+    
     @After
     public void cleanUpStreams() {
         System.setOut(null);
